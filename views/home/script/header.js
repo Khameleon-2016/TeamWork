8ea956050f914cb1f09aee3e9a4ce9c22bbc0d8c
Westelf <Westelf@users.noreply.github.com>
2016-09-23 20:16:38 +0300

    Update - Home button

--- views/home/script/header.js
+++ views/home/script/header.js
@@ -1,4 +1,13 @@
 
+//Function that loads initial content
+function loadInitialContent() {
+    $mainSection = $('.base-docs-section');
+
+    $mainSection.load('home.html');
+
+    setTimeout(loginAndRegisterButtons, 100);
+}
+
 function attachHeaderBtnsFunction () {
     let $activeListItem, $btnsList, $btnsListItems, $btns, i, len;
 
@@ -7,22 +16,35 @@ function attachHeaderBtnsFunction () {
     $btnsListItems = $btnsList.children();
 
     $activeListItem = $btnsList.find('.active').eq(0);
-    console.log($activeListItem);
 
     $btns = $btnsList.find('a');
 
+    //Function that changes the color of the button that correspond to the active page
+    function setActivePageButtonColor ($target) {
+        
+        if (!$target.parent().hasClass('active'))
+        {
+            $target.parent().addClass('active');
+
+            $activeListItem.removeClass('active');
+
+            $activeListItem = $target.parent();
+        }
+            
+    }
+
+    //Set event listeners to header buttons
     $btns.each(function () {
         let thisNode = $(this);
 
         thisNode.on('click', function (event) {
             var $target = $(event.target);
 
-            $target.parent().addClass('active');
+            setActivePageButtonColor($target);
 
-            $activeListItem.removeClass('active');
-
-            $activeListItem = $target.parent();
-            console.log($activeListItem);
+            if ($target.attr('id') === 'home-btn') {
+                loadInitialContent();
+            }
         });
     });
 }
