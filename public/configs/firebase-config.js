7930b410dd8849d47a6527d298f1a7b6a9d7c1d3
Dimitar Stoqnov <stoyanov.dimi.tri@gmail.com>
2016-09-27 11:55:43 +0300

    I started to editing a sign up functionality.

---
+++ public/configs/firebase-config.js
@@ -0,0 +1,9 @@
+ var config = {
+    apiKey: "AIzaSyCMKwdSlr2UTyg_Yip5CUvQvIId94qfuOY",
+    authDomain: "teamkhameleon.firebaseapp.com",
+    databaseURL: "https://teamkhameleon.firebaseio.com",
+    storageBucket: "teamkhameleon.appspot.com",
+    messagingSenderId: "818068003629"
+  };
+  
+  firebase.initializeApp(config);
