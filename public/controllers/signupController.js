a8d7c06acc1652a87321c5b084dd509256f13ac1
Dimitar Stoqnov <stoyanov.dimi.tri@gmail.com>
2016-09-27 14:15:13 +0300

    I added functionality for sign up.

--- public/controllers/signupController.js
+++ public/controllers/signupController.js
@@ -1,13 +1,29 @@
 const signupController = function () {
     var $email,
-        $password;
-        //auth = firebase.auth();
+        $password,
+        auth = firebase.auth();
 
     $.ajax({
         url: 'templates/sign-up-form.html',
         type: 'GET'
     }).done((template) => {
         $('.content').html(template);
+
+
+        $('.signup').on('click', () => {
+
+            $email = $('.email').val();
+            $password = $('.password').val();
+
+            auth.createUserWithEmailAndPassword($email, $password)
+            .catch((error) => {
+                // TODO: Dimitar : Have to do when the user want to sign up but he has invalid data (email, password) to show a proper message.
+                 
+                console.log($email);
+                console.log(error.code);
+                console.log(error.message);
+            });
+        });
     });
 };
 
