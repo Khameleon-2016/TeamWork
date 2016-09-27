43b66d808d562deea8c24646c4ecaf4b8368762e
Dimitar Stoqnov <stoyanov.dimi.tri@gmail.com>
2016-09-27 14:54:58 +0300

    I finished to edit the log in functionality.

--- public/controllers/signinController.js
+++ public/controllers/signinController.js
@@ -1,5 +1,28 @@
 const signinController = function() {
+    var $email,
+        $password,
+        auth = firebase.auth();
 
+    $.ajax({
+        url: 'templates/sign-in-form.html',
+        type: 'GET'
+    }).done((template) => {
+        $('.content').html(template);
+        
+        $email = $('.email').val();
+        $password = $('.password').val();
+
+        $('.signin').on('click', () => {
+            
+            auth.signInWithEmailAndPassword($email, $password)
+            .catch((error) => {
+                console.log(error.code);
+                console.log(error.message);
+            });
+
+            console.log("sdffd");
+        });
+    });
 };
 
 export { signinController };
