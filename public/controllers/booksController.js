5aa782a1fb8f7296804a3b9c06b36233010fa9bd
Todor Enikov <enigmaenikov@gmail.com>
2016-09-28 13:41:30 +0300

    Added functionality on booksController.js.

--- public/controllers/booksController.js
+++ public/controllers/booksController.js
@@ -1,5 +1,18 @@
-const booksController = {
+const booksController = function() {
+    var books = firebase.database().ref('/books'),
+        compile;
 
+    $.ajax({
+        url: 'templates/books-template.handlebars',
+        type: 'GET'
+    }).done((template) => {
+
+        books.on('value', (data) => {
+            compile = Handlebars.compile(template);
+
+            $('.content').html(compile(data.val()));
+        });
+    });
 };
 
-export { booksController }
+export { booksController };
