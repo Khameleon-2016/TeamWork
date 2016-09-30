7e75489205936cd3555e12b5f7cd59feb5939ce4
Todor Enikov <enigmaenikov@gmail.com>
2016-09-30 13:04:08 +0300

    Removed unnecessary code from testscontrollers.js file.

--- public/unit tests/testscontrollers.js
+++ public/unit tests/testscontrollers.js
@@ -1,5 +1,5 @@
 chai.should();
-var firebase = ' sda';
+
 describe('Test all of the controllers in the application', function() {
 
     it('Authors controller should be a function', function() {
@@ -38,54 +38,3 @@ describe('Test all of the controllers in the application', function() {
     });
 
 });
-
-// describe('Post request tests', function() {
-//     beforeEach(function() {
-//         this.xhr = sinon.useFakeXMLHttpRequest();
-
-//         this.requests = [];
-//         this.xhr.onCreate = function(xhr) {
-//             this.requests.push(xhr);
-//         }.bind(this);
-//     });
-
-//     it('The post request should return an object', function(done) {
-//         var url = 'templates/authors-template.handlebars';
-//         var body = { name: 'Jorkata' };
-//         var result = requester.post(url, body);
-//         done();
-//         expect(result).to.be.an('object');
-
-//     });
-
-//     it('The post request should return object with property content-type equal to application/json', function(done) {
-//         var url = 'templates/authors-template.handlebars';
-//         var body = { name: 'Jorkata' };
-
-//         requester.post(url, body);
-//         done();
-//         this.requests[0].respond(200, { 'Content-Type': 'application/json' });
-//     });
-
-//     it('The post request should return object with property url equal to the passed url in the post function', function(done) {
-//         var url = 'templates/authors-template.handlebars';
-//         var body = { name: 'Jorkata' };
-
-//         requester.post(url, body);
-//         done();
-//         this.requests[0].respond(200, { 'url': 'templates/authors-template.handlebars' });
-//     });
-
-//     it('The post request should return object with property method which has value POST', function(done) {
-//         var url = 'templates/authors-template.handlebars';
-//         var body = { name: 'Jorkata' };
-
-//         requester.post(url, body);
-//         done();
-//         this.requests[0].respond(200, { 'method': 'POST' });
-//     });
-
-//     afterEach(function() {
-//         this.xhr.restore();
-//     });
-// });
