fd513d1adff67b1aa45d4532205d406e060053a0
Nikolai <fester.s.son@gmail.com>
2016-10-03 02:10:01 +0300

    Categories functionality added

--- public/controllers/categoriesController.js
+++ public/controllers/categoriesController.js
@@ -1,16 +1,55 @@
 import { requester } from '../http-request/request.js';
 
 var categoriesController = function() {
+    var allBooks = [],
+        booksByGenres = [],
+        genres = [];
 
     requester
-        .get('templates/categories.html')
+        .get('templates/categories.handlebars')
         .then((template) => {
-            $('.content').html(template);
-            var result = firebase.database().ref().child('books');
-            console.log(result);
-        });
+            let dbObject = firebase.database().ref().child('books');
+            dbObject.on('value', function(el) {
+            var books = el.val();
 
-    
+            books.forEach(function(book) {
+                book.genre.forEach(function(genre) {
+                    if (!booksByGenres[genre]) {
+                        booksByGenres[genre] = [];
+                        genres.push(genre);
+                    }
+
+                    booksByGenres[genre].push(book);
+                });
+            });
+
+            var compile = Handlebars.compile(template);
+            var compiled = compile({
+                books, 
+                genres: genres
+            });
+
+            $('.content').html(compiled);
+            
+            var btn = document.getElementById('select-genre');
+            var select = document.getElementById('genre');
+            btn.addEventListener('click', function() {
+                let selectedGenre = select.options[select.selectedIndex].text,
+                    booksForCurrentGenre = booksByGenres[selectedGenre];
+
+                requester
+                .get('templates/categories-books.handlebars')
+                .then((template) => {
+                    var compile = Handlebars.compile(template);
+                    var compiled = compile({
+                        books: booksForCurrentGenre
+                    });
+
+                    $('#result-container').html(compiled);
+                });
+            });
+        });
+    });
 };
 
 export { categoriesController };
